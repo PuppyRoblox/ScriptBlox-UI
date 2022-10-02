aea7e74b398b1ef08ba369b663d69770f4cb25f9
PuppyRoblox <113235599+PuppyRoblox@users.noreply.github.com>
2022-10-02 08:56:27 +0100

    Update ui_main.lua

--- ui_main.lua
+++ ui_main.lua
@@ -68,7 +68,7 @@ function module:Init()
     Header_2.Size = UDim2.new(0, 468, 0, 70)
     Header_2.Font = Enum.Font.Highway
     Header_2.RichText = true
-    Header_2.Text = "<font color=\"rgb(170, 0, 255)\">Script</font>Blox"
+    Header_2.Text = "Script"
     Header_2.TextColor3 = Color3.fromRGB(255, 255, 255)
     Header_2.TextSize = 68.000
 
